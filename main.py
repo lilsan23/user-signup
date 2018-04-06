209a51e99dc436815605a486a3332cc8ce2990b2
Sandra Edwards <lilsan23@aol.com>
2018-04-05 21:33:59 -0500

    created sign up form

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__) 
 
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
+@app.route("/", methods= ['Get','Post'])
+def create_username():
     return render_template('signup.html')
+app.run()
