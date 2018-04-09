f620205d1bd2280578e445ffc075d439231445a5
Sandra Edwards <lilsan23@aol.com>
2018-04-08 22:27:47 -0500

    Added first error message

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/", methods= ['Get','Post'])
-def create_username():
+@app.route("/")
+def create_signup():
     return render_template('signup.html')
+
+@app.route("/", methods=['POST'])
+def hello():
+    user_id = request.Form('username')
+
+
+    if len(user_id) <3 or len(user_id)==0:
+        userid_error = "Username does not meet requirements. Try again"
+        user_id=""
+        return userid_error
+    else:
+    return render_template('hello.html', username=user_id)
+
+    if 
+
 app.run()
