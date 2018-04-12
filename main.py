89e8b63fe8e89f9afce1ceaee0cab6cfe132d4d1
Sandra Edwards <lilsan23@aol.com>
2018-04-11 21:52:26 -0500

    refactored code

--- main.py
+++ main.py
@@ -17,20 +17,27 @@ def hello():
     if len(user_id)<3 or user_id =="" or (" " in user_id):
         userid_error = "Username does not meet requirements. Try again."
         user_id=""
-        return render_template('signup.html', userid_error=userid_error)
-        
+            
     if (not '@' in email) or (not '.' in email):
         email_error = "Email should contain '@' and '.'."
         email=""
-        return render_template('signup.html', email_error=email_error)
-        
+            
     if len(email) <3 or len(email) >20:
         email2_error = "Email must be between 3 and 20 characters."
         email="" 
-        return render_template('signup.html', email2_error=email2_error)
-
+    
+    if not userid_error or not email_error or not email2_error:
+        return render_template('hello.html', username=user_id)
+      
     else:
-        return render_template('hello.html', username=user_id,email=email)
+        return render_template('signup.html', userid_error=userid_error, 
+            email_error=email_error, 
+            email2_error=email2_error,
+            user_id=user_id,
+            email=email)
+
+   
+    
 
     # else:
     # return render_template('signup.html', userid_error=userid_error, password_error= password_error, 
