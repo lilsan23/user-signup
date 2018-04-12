64f675a55f1e5f01434cb165dff1ffe5fd54bdee
Sandra Edwards <lilsan23@aol.com>
2018-04-12 00:05:26 -0500

    combined coded

--- main.py
+++ main.py
@@ -12,28 +12,44 @@ def create_signup():
 @app.route("/", methods=['POST'])
 def hello():
     user_id = request.form['username']
+    pass_word = request.form['password']
+    ver_password = request.form['verify_password']
     email = request.form['email']
 
     if len(user_id)<3 or user_id =="" or (" " in user_id):
         userid_error = "Username does not meet requirements. Try again."
 
+    if len(pass_word) <3 or len(pass_word) >8:
+        password_error = "Password does not meet requirements. Try again."
+        pass_word=""
+    
+    if not ver_password == pass_word: 
+        ver_password_error = "Password does not match."
+        ver_password="" 
             
     if (not '@' in email) or (not '.' in email):
         email_error = "Email should contain '@' and '.'."
         email=""
             
-    if len(email) <3 or len(email) >20:
+    if len(email) <3 and email!="" or len(email) >20 and email!="":
         email2_error = "Email must be between 3 and 20 characters."
         
-    
+    if not userid_error or password_error or email_error or email2_error:
+        return render_template('hello.html', username=user_id)
+
+    else:
         return render_template('signup.html', userid_error=userid_error, 
+            password_error=password_error, 
+            ver_password_error=ver_password_error,
             email_error=email_error, 
             email2_error=email2_error,
             user_id=user_id,
+            password=pass_word,
+            verify_password=ver_password,
             email=email)
       
-    else:
-        return render_template('hello.html', username=user_id)
+
+        
 
    
     
