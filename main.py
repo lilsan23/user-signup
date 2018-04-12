3cdd77432a4f776ae9ad948e389e214f701a4a16
Sandra Edwards <lilsan23@aol.com>
2018-04-11 21:36:22 -0500

    semi working code

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ def create_signup():
 @app.route("/", methods=['POST'])
 def hello():
     user_id = request.form['username']
-    
-    if len(user_id)<3:
+    email = request.form['email']
+
+    if len(user_id)<3 or user_id =="" or (" " in user_id):
         userid_error = "Username does not meet requirements. Try again."
         user_id=""
-        
-    if not userid_error:
-        return render_template('hello.html', username=user_id)
-    
-    else:
         return render_template('signup.html', userid_error=userid_error)
+        
+    if (not '@' in email) or (not '.' in email):
+        email_error = "Email should contain '@' and '.'."
+        email=""
+        return render_template('signup.html', email_error=email_error)
+        
+    if len(email) <3 or len(email) >20:
+        email2_error = "Email must be between 3 and 20 characters."
+        email="" 
+        return render_template('signup.html', email2_error=email2_error)
+
+    else:
+        return render_template('hello.html', username=user_id,email=email)
 
     # else:
     # return render_template('signup.html', userid_error=userid_error, password_error= password_error, 
@@ -31,6 +40,4 @@ def hello():
     #     password=pass_word)
     
  
-  
-
 app.run()
